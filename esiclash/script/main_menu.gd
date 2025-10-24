6b58cffae08accecdcb4849db9e0e684bb9509f0
mamat2525 <mathieu.jacque25@gmail.com>
2025-10-24 20:08:01 +0200

    temp

--- esiclash/script/main_menu.gd
+++ esiclash/script/main_menu.gd
@@ -2,11 +2,16 @@ extends Node2D
 
 func _ready():
 	
-	get_window().set_size(Vector2(1280,720))
+	
 	var args = OS.get_cmdline_args()
+	await get_tree().process_frame
+	get_window().set_size(Vector2(1600,920))
+	get_window().set_mode(Window.MODE_WINDOWED)
+	get_window().set_position(Vector2(320, 180))
 	if args[1] == "serveur":
-		await get_tree().process_frame
+		get_window().set_mode(Window.MODE_MINIMIZED)
 		get_tree().change_scene_to_file("res://scene/serveur.tscn")
+		
 
 func _on_boutton_jouer_button_down() -> void:
 	get_tree().change_scene_to_file("res://scene/waitingScene.tscn")
